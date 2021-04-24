706c5cfb47d433710c7f3af20799ebe5486d82da
shivam satpute <shivam897satpute@gmail.com>
2021-04-24 16:51:04 +0530

    Added: compute a*b+c

--- sortingArithmetic.sh
+++ sortingArithmetic.sh
@@ -10,7 +10,9 @@ read c
 
 #compute
 
-operation=`expr $a + $b \* $c`
+operation1=`expr $a + $b \* $c`
+operation2=`expr $a \* $b + $c`
 
 echo "Three inputs are :" $a $b $c
-echo "Operation:" $operation
+echo "Operation1:" $operation1
+echo "Operation2:" $operation2
